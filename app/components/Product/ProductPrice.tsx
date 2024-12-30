56e36ff158b83cc5d9dd21c5aec517c56306f095
Gabriel Oporto <gabyoporto@gmail.com>
2024-12-30 09:33:38 -0300

    refactor: optimize formatting logic with useMemo to improve performance

--- app/components/Product/ProductPrice.tsx
+++ app/components/Product/ProductPrice.tsx
@@ -3,12 +3,25 @@ import {
   calculateSavingsPercentage,
   formatPrice,
 } from "@/app/utils/productUtils";
+import { useMemo } from "react";
 
 interface ProductPriceProps {
   product: Product;
 }
 
 export default function ProductPrice({ product }: ProductPriceProps) {
+  const savingsPercentage = useMemo(
+    () => calculateSavingsPercentage(product),
+    [product]
+  );
+
+  const price = useMemo(() => formatPrice(product.listingPrice!), [product]);
+
+  const unitPrice = useMemo(
+    () => product.price / product.unitValue!,
+    [product]
+  );
+
   return (
     <div className="mb-2">
       <div className="flex flex-col py-2">
@@ -18,18 +31,16 @@ export default function ProductPrice({ product }: ProductPriceProps) {
               <p className="text-xl font-bold">{formatPrice(product.price)}</p>
               {product.listingPrice && (
                 <span className="text-xs text-white bg-secondary rounded-lg py-1 px-3">
-                  {calculateSavingsPercentage(product)}
+                  {savingsPercentage}
                 </span>
               )}
             </div>
             {product.salesUnit === "group" && product.unitValue && (
-              <p className="text-xs text-gray-600 font-bold">
-                PU: {product.price / product.unitValue}
-              </p>
+              <p className="text-xs text-gray-600 font-bold">PU: {unitPrice}</p>
             )}
             {product.listingPrice && (
               <p className="text-sm text-gray-400 line-through mb-2 font-normal">
-                {formatPrice(product.listingPrice)}
+                {price}
               </p>
             )}
           </div>
