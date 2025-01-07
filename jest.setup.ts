a5abdb20109e7f17cd33708413642b88f82c19c3
Gabriel Oporto <gabyoporto@gmail.com>
2025-01-07 12:24:34 -0300

    fix: ensure consistent currency formatting in tests

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,19 @@
 import "@testing-library/jest-dom";
+
+// Establece la configuración regional a "es-AR" para todos los tests
+const mockNumberFormat = jest.fn().mockImplementation(() => {
+  return {
+    format: (value: number) =>
+      new Intl.NumberFormat("es-AR", {
+        style: "currency",
+        currency: "ARS",
+        maximumFractionDigits: 0,
+      })
+        .format(value)
+        .replace(/\s/g, ""),
+  };
+}) as unknown as typeof Intl.NumberFormat;
+
+mockNumberFormat.supportedLocalesOf = jest.fn().mockReturnValue(["es-AR"]);
+
+global.Intl.NumberFormat = mockNumberFormat;
